c90ff700c098b7ab6bde037b0a05ce4053ec70d4
srlane <scott.lane@ciholas.com>
2019-11-05 11:49:50 -0600

    Added view for friends search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No users matched this search criteria" if @users.blank?
+    end
+    respond_to do |format|
+      format.js {render partial: 'friends/result'}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :friendships
-
   get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 end
